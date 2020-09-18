e9141c861759d816b433ff9c71621a57e5154656
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-09-18 23:22:58 +0300

    example code with generated grpc client

---
+++ src/AnalogyClient.py
@@ -0,0 +1,43 @@
+import grpc
+import Analogy_pb2
+import Analogy_pb2_grpc
+
+
+def make_message(message):
+    return Analogy_pb2.AnalogyLogMessage(
+        Text=message
+    )
+
+
+def generate_messages():
+    messages = [
+        make_message("First message"),
+        make_message("Second message"),
+        make_message("Third message"),
+        make_message("Fourth message"),
+        make_message("Fifth message"),
+    ]
+    for msg in messages:
+        print("Hello Server Sending you the %s" % msg.Text)
+        yield msg
+
+
+def send_message(stub):
+    stub.SubscribeForSendMessages(generate_messages())
+    print("Done")
+
+
+def run():
+
+    # open a gRPC channel
+    channel = grpc.insecure_channel('localhost:6000')
+
+    # create a stub (client)
+    stub = Analogy_pb2_grpc.AnalogyStub(channel)
+
+    send_message(stub)
+
+
+
+if __name__ == "__main__":
+    run()
